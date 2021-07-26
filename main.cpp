9f6483d2ff9be0fe3e433d6ff3277d1f15e4abd2
Karthik <karthik201024@gmail.com>
2021-07-26 21:08:56 +0530

    Added functionality to the code

--- main.cpp
+++ main.cpp
@@ -1,64 +1,89 @@
+// Game created by Karthik, inspired by 'NVitanovic'
 #include <iostream>
 using namespace std;
-bool gameOver;
-const int width = 20;
-const int height = 20;
-int x, y, fruitX, fruitY, score;
-enum eDirecton
+// FOREVER VARIABLES DO NOT CHANGE!!
+enum eDir {STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
+// Class "cBall", contains core code for the ball, contains functions for the screen and ball
+class cBall
 {
-    STOP = 0,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
+private:
+	int x, y;
+	int originalX, originalY;
+	eDir direction;
+public:
+	cBall(int posX, int posY)
+	{
+		originalX = posX;
+		originalY = posY;
+		x = posX; y = posY;
+		direction = STOP;
+
+	}
+	void Reset()
+	{
+		x = originalX; y = originalY;
+		direction = STOP;
+
+	}
+	void changeDirection(eDir d)
+	{
+		direction = d;
+
+	}
+	void randomDirection()
+	{
+		direction = (eDir)((rand() % 6) + 1);
+	}
+	inline int getX() { return x; }
+	inline int getY() { return y; }
+	inline eDir getDirection() { return direction; }
+	// Functionality 
+	void Move()
+	{
+		switch (direction)
+		{
+		case STOP:
+			break;
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UPLEFT:
+			x--; y--;
+			break;
+		case DOWNLEFT:
+			x--; y++;
+			break;
+		case UPRIGHT:
+			x++; y--;
+			break;
+		case DOWNRIGHT:
+			x++; y++;
+			break;
+		default:
+			break;
+		}
+	}
+	friend ostream& operator<<(ostream& o, cBall c)
+	{
+		o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]" << endl;
+		return o;
+	}
 };
-eDirecton dir;
-void Setup()
-{
-    gameOver = false;
-    dir = STOP;
-    x = width / 2;
-    y = height / 2;
-    fruitX = rand() % width;
-    fruitY = rand() % height;
-    score = 0;
-}
-void Draw()
-{
-    system("cls");
-    for (int i = 0; i < width + 2; i++)
-        cout << "#";
-    cout << endl;
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (j == 0)
-                cout << "#";
-
-            cout << " ";
-
-            if (j == width - 1)
-                cout << "#";
-        }
-        cout << endl;
-    }
-}
-void Input()
-{
-}
-void Logic()
-{
-}
+// Main loop
 int main()
 {
-    Setup();
-    while (!gameOver)
-    {
-        Draw();
-        Input();
-        Logic();
-    }
-    return 0;
+	cBall c(0, 0);
+	cout << c << endl;
+	c.randomDirection();
+	cout << c << endl;
+	c.Move();
+	cout << c << endl;
+	c.randomDirection();
+	c.Move();
+	cout << c << endl;
+	
+	return 0;
 }
